bab7f9da102c33a5705e50c587c41bdd20158fe9
april_chou <632549546@qq.com>
2017-07-01 21:27:50 +0800

    testAppium

--- src/main/java/test/CaocaoDriverTest.java
+++ src/main/java/test/CaocaoDriverTest.java
@@ -1,8 +1,12 @@
+package test;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
+import commonController.SwipeTo;
+import io.appium.java_client.android.AndroidElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -15,39 +19,41 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class CalculatorTest {
-	private static AppiumDriver driver;
+public class CaocaoDriverTest {
+//	private static AppiumDriver driver;
+	private static AndroidDriver<AndroidElement> driver;
+
 	@BeforeClass
 	public static void setUp() throws MalformedURLException{
 		DesiredCapabilities caps = new DesiredCapabilities();
 		caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "5.1.1");
 		caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-		caps.setCapability(MobileCapabilityType.DEVICE_NAME, "demo");
-		caps.setCapability("appPackage", "com.android.calculator2");
-		caps.setCapability("appActivity", ".Calculator");
+		caps.setCapability(MobileCapabilityType.DEVICE_NAME, "April");
+		caps.setCapability("appPackage", "cn.caocaokeji.driver");
+		caps.setCapability("appActivity", ".activity.ActLoading");
 		driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);
 	}
-	
-	@Test
-	public void testmul() throws IOException{
-		WebElement num9 = driver.findElement(By.name("9"));
-		num9.click();
-		
-		WebElement mul = driver.findElement(By.name("×"));
-		mul.click();
-		
-		WebElement num2 = driver.findElement(By.name("2"));
-		num2.click();
-		
-		WebElement equal = driver.findElement(By.name("equals"));
-		equal.click();
-		
-	    WebElement result = driver.findElement(By.className("android.widget.EditText"));
-		Assert.assertEquals(result.getText(), "18",  "输出结果错误！");
 
-		System.out.println("输出结果正确！");
+	@Test
+	public void test() throws IOException{
+
+		for(int i = 0;i < 3;i ++) {
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+
+			}
+//		SwipeTo.swipeToLeft(driver,60,3);
+			driver.swipe(450, 300, 50, 300, 500);
+		}
+
+		WebElement startWork = driver.findElement(By.id("cn.caocaokeji.driver:id/btn_start_experience"));
+		startWork.click();
+		WebElement register = driver.findElement(By.id("cn.caocaokeji.driver:id/bt_sign_up"));
+		register.click();
+
 	}
-	
+
 	@AfterClass
 	public static void tearDown(){
 		driver.quit();
